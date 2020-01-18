6026f2413067655d8156e41a40778555edc9eb6c
Stef Vercaemer <stefaan.vercaemer@howest.be>
2020-01-18 12:33:13 +0100

    Add project files.

---
+++ Fietsen.Lib/Entities/Fiets.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fietsen.Lib.Entities
+{
+    public class Fiets
+    {
+        public Guid Id { get; set; }
+
+        public int AantalWielen { get; set; }
+
+        public DateTime AankoopDatum { get; set; }
+
+        public bool IsElektrisch { get; set; }
+
+        public string Merk { get; set; }
+
+        public float Snelheid { get; set; }
+    }
+}
