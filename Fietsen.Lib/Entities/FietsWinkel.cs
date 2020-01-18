dd5c64bdc2ccd8924b278e851e981876711fc112
Stef Vercaemer <stefaan.vercaemer@howest.be>
2020-01-18 12:37:23 +0100

    Add FietsWinkel

---
+++ Fietsen.Lib/Entities/FietsWinkel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fietsen.Lib.Entities
+{
+    public class FietsWinkel
+    {
+        public string Naam { get; set; }
+        public List<Fiets> Fietsen { get; set; }
+
+        public FietsWinkel(string naam)
+        {
+            Fietsen = new List<Fiets>();
+            Naam = naam;
+        }
+
+        public override string ToString()
+        {
+            return $"{Naam} {Fietsen.Count} fietsen";
+        }
+
+    }
+}
